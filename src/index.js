5ac4c96f3c49d32b860770d328622581531c217b
aditya patel <adityapatel@adityas-MacBook-Pro.local>
2022-10-02 13:46:46 +0530

    firebase setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello from Bangalore');
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNP71YDQmYOTgYA6rrZzH6QR_-eqZBQq8',
+  authDomain: 'fir-dojo-e9828.firebaseapp.com',
+  projectId: 'fir-dojo-e9828',
+  storageBucket: 'fir-dojo-e9828.appspot.com',
+  messagingSenderId: '544388767414',
+  appId: '1:544388767414:web:621efdf30115d160811017'
+};
+
+initializeApp(firebaseConfig);
