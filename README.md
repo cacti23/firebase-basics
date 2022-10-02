f00f7bec24ac72142b6ef61c4b14995e9c6aa7b2
aditya patel <adityapatel@adityas-MacBook-Pro.local>
2022-10-02 17:50:44 +0530

    firestore initialized

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-##Steps:
+## Steps
 
-1. git init and npm init -y
-2. npm install webpack webpack-cli
-3. Create two folders dist and src
-4. Inside dist make index.html file and link it to bunlde.js file which will the webpack output
-5. Insde src folder make index.js file and write some code
-6. After that create webpack.config.js with required configurations
-7. Also add a build script in package.json file
+1. go to firebase website and click on firestore to proceed forward
+2. after configuration on website you can check index.js file in the branch in order to understand how to interact with the db.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBNP71YDQmYOTgYA6rrZzH6QR_-eqZBQq8',
@@ -9,4 +10,24 @@ const firebaseConfig = {
   appId: '1:544388767414:web:621efdf30115d160811017'
 };
 
+// init firebase app
 initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'books');
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
