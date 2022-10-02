1fdf31f5d256b17737c3497b82f039e69c327453
aditya patel <adityapatel@adityas-MacBook-Pro.local>
2022-10-02 18:07:42 +0530

    documents can be added and deleted

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ## Steps
 
-1. go to firebase website and click on firestore to proceed forward
-2. after configuration on website you can check index.js file in the branch in order to understand how to interact with the db.
+1. checkout index.js inside src
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBNP71YDQmYOTgYA6rrZzH6QR_-eqZBQq8',
@@ -31,3 +38,28 @@ getDocs(colRef)
   .catch((err) => {
     console.log(err.message);
   });
+
+// adding documents
+const addBookForm = document.querySelector('.add');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete');
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value);
+
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
+});
